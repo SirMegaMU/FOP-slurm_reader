4e3409ff815dda8bd6023934356d24ddcf9c9c41
MegaMU <sirmegamu@outlook.com>
2023-01-23 17:22:09 +0800

    add regex patterns

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,5 @@
+package src.main;
+
 import tech.tablesaw.api.IntColumn;
 import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
@@ -22,21 +24,57 @@ public class Main {
             Scanner file_scanner = new Scanner(new File("./extracted_log"));
             while (file_scanner.hasNextLine()) {
                 String line = file_scanner.nextLine();
+                int JobId = 0, InitPrio = 0, usec = 0, uid = 0, CPUs = 0;
+                String NodeList, Partition, status;
+                Date start, end;
+                Matcher m;
 
                 // [2022-06-01T01:02:35.148] _slurm_rpc_submit_batch_job: JobId=42802 InitPrio=19758 usec=589
-                Pattern submit = Pattern.compile("\\[(.*T.*)\\] _slurm_rpc_submit_batch_job: JobId=([0-9]*) InitPrio=([0-9]*) usec=([0-9]*)\s");
+                String submit_p = "\\[(.*T.*)\\] _slurm_rpc_submit_batch_job: JobId=([0-9]*) InitPrio=([0-9]*) usec=([0-9]*)\s";
+                Pattern submit = Pattern.compile(submit_p);
                 // [2022-06-01T01:02:36.012] sched: Allocate JobId=42802 NodeList=gpu05 #CPUs=32 Partition=gpu-v100s
-                Pattern sched = Pattern.compile("\\[(.*T.*)\\] sched: Allocate JobId=([0-9]*) NodeList=(.*?) #CPUs=([0-9]*) Partition=(.*)\s");
+                String sched_p = "\\[(.*T.*)\\] sched: Allocate JobId=([0-9]*) NodeList=(.*?) #CPUs=([0-9]*) Partition=(.*)\s";
+                Pattern sched = Pattern.compile(sched_p);
                 //[2022-06-01T04:05:04.581] _job_complete: JobId=42801 WEXITSTATUS 2
-                Pattern complete = Pattern.compile("\\[(.*T.*)\\] _job_complete: JobId=[0-9]* (.*)\s");
+                String complete_p = "\\[(.*T.*)\\] _job_complete: JobId=[0-9]* (.*)\s";
+                Pattern complete = Pattern.compile(complete_p);
                 // [2022-06-01T15:12:23.290] error: This association 187(account='free', user='lobbeytan', partition='(null)') does not have access to qos long
-                Pattern error = Pattern.compile("\\[(.*T.*)\\] error: This association ([0-9]*)\\(account='(.*)', user='(.*)', partition='(.*)'\\) (.*?)\s");
+                String error_p = "\\[(.*T.*)\\] error: This association ([0-9]*)\\(account='(.*)', user='(.*)', partition='(.*)'\\) (.*?)\s";
+                Pattern error = Pattern.compile(error_p);
                 // [2022-06-01T10:39:24.178] _slurm_rpc_kill_job: REQUEST_KILL_JOB JobId=42819 uid 548200029
-                Pattern kill = Pattern.compile("\\[(.*T.*)\\] _slurm_rpc_kill_job: REQUEST_KILL_JOB JobId=([0-9]*) uid ([0-9]*)]\s");
+                String kill_p = "\\[(.*T.*)\\] _slurm_rpc_kill_job: REQUEST_KILL_JOB JobId=([0-9]*) uid ([0-9]*)]\s";
+                Pattern kill = Pattern.compile(kill_p);
 
-                //slurm_job job = new slurm_job();
+                if (Pattern.matches(submit_p, line)) {
+                    m = submit.matcher(line);
+                    JobId = Integer.parseInt(m.group(0));
+                    InitPrio = Integer.parseInt(m.group(1));
+                    usec = Integer.parseInt(m.group(2));
+                } else if (Pattern.matches(sched_p, line)) {
+                    m = sched.matcher(line);
+                    JobId = Integer.parseInt(m.group(0));
+                    NodeList = m.group(1);
+                    CPUs = Integer.parseInt(m.group(2));
+                    Partition = m.group(3);
+                } else if (Pattern.matches(complete_p, line)) {
+                    m = complete.matcher(line);
+                    JobId = Integer.parseInt(m.group(0));
+                    status = m.group(1);
+                } else if (Pattern.matches(error_p, line)) {
+                    m = kill.matcher(line);
+                    JobId = Integer.parseInt(m.group(0));
+                    uid = Integer.parseInt(m.group(1));
+                } else if (Pattern.matches(kill_p, line)) {
+                }
+                if (JobId != 0) {
+                    if (job_map.containsKey(JobId)) {
 
-                //job_map.put();
+                    } else {
+                        slurm_job job = new slurm_job(JobId);
+                        job_map.put(JobId, job);
+                    }
+                }
+
 
             }
             file_scanner.close();
@@ -53,7 +91,7 @@ public class Main {
             IntColumn.create("usec"),
             IntColumn.create("uid"),
             StringColumn.create("NodeList"),
-            StringColumn.create("CPUs"),
+            IntColumn.create("CPUs"),
             StringColumn.create("Partition"),
             StringColumn.create("status"),
             StringColumn.create("start"),
@@ -61,8 +99,8 @@ public class Main {
     );
 
     public class slurm_job {
-        int JobId, InitPrio, usec, uid;
-        String job, NodeList, CPUs, Partition, status;
+        int JobId, InitPrio, usec, uid, CPUs;
+        String job, NodeList, Partition, status;
         Date start, end;
 
         public slurm_job(int JobId) {
@@ -88,7 +126,7 @@ public class Main {
                     IntColumn.create("usec", new int[]{this.usec}),
                     IntColumn.create("uid", new int[]{this.uid}),
                     StringColumn.create("NodeList", new String[]{this.NodeList}),
-                    StringColumn.create("CPUs", new String[]{this.CPUs}),
+                    IntColumn.create("CPUs", new int[]{this.CPUs}),
                     StringColumn.create("Partition", new String[]{this.Partition}),
                     StringColumn.create("status", new String[]{this.status}),
                     StringColumn.create("start", new String[]{this.start.toString()}),
